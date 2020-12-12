68091f49f6c525e90384b1ec4c9ab6ae5820a04b
Mbekezeli Sithole <mbekemuhle@gmail.com>
2020-12-12 09:39:05 +0200

    header 1

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB9Ndm5kWQpjUBp_3mu1k72McyJbiWMPY4",
+    authDomain: "clone-44166.firebaseapp.com",
+    projectId: "clone-44166",
+    storageBucket: "clone-44166.appspot.com",
+    messagingSenderId: "513291248153",
+    appId: "1:513291248153:web:ceb7e9f3949688c1438088",
+    measurementId: "G-5EQE3RZKJX"
+  };
