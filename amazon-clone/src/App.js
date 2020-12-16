731fd16787677d515624e895812cb72c50ef448a
Mbekezeli Sithole <mbekemuhle@gmail.com>
2020-12-16 13:07:53 +0200

    payment frontend

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import Header from './Header'
 import Login from './Login'
 import Home from './Home'
+import Payment from './Payment'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Checkout from './Checkout';
 import { auth } from './firebase'
 import { useStateValue } from './StateProvider';
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+
+const promise = loadStripe('pk_test_51HyuWzAdmfnlfY7bHj3hu8ZJI0uLl7cp5d16zzWi46jGUS52tRWAzgDqnG4D1Kc3jxenE23FBzlXf8s4HXCySN1500nON5Bdd4')
 
 function App() {
   const [{}, dispatch] = useStateValue()
@@ -45,6 +50,13 @@ function App() {
           <Route path='/checkout'>
             <Header />
             <Checkout />
+          </Route>
+
+          <Route path='/payment'>
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
 
           <Route path='/'> 
